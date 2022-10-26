8df7c3caf7208c09ea86f1019e2e7bbf2daa33e1
aminul <accounreate@gmail.com>
2022-10-26 21:01:13 +0600

    course card added

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 app.use(cors());
+const classes = require('./data/courseCard.json')
 
 const port = process.env.PORT || 5000;
 
 app.get("/", (req, res)=>{
    res.send("server is run");
+})
+
+app.get("/classes", (req, res)=>{
+   res.send(classes);
 })
 
 app.listen(port, ()=>{
